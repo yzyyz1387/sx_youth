c9610613a0cb14ffd09ed8a1c7aacd8ab7db0eb9
yzyyz1387 <1796031384@qq.com>
2022-11-20 00:47:40 +0800

    :wrench: Tools

---
+++ youth/utils.py
@@ -0,0 +1,207 @@
+# python3
+# -*- coding: utf-8 -*-
+# @Time    : 2022/8/14 13:17
+# @Author  : yzyyz
+# @Email   :  youzyyz1384@qq.com
+# @File    : utils.py
+# @Software: PyCharm
+import json
+import logging
+import os
+import sys
+import signal
+from openpyxl import load_workbook
+from functools import wraps
+from os.path import dirname
+from typing import Optional, Union
+from playwright.async_api import async_playwright, Browser, BrowserContext, Page
+
+import aiofiles
+import yaml
+from nonebot import logger, get_driver
+from jinja2 import Environment, FileSystemLoader
+from nonebot.adapters.onebot.v11 import Event, GroupMessageEvent, MessageSegment, PrivateMessageEvent
+from playwright.async_api import async_playwright, Browser
+
+from .path import *
+
+
+def log():
+    """
+    日志
+    :return:
+    """
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter('%(asctime)s-%(levelname)s: %(message)s')
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.formatter = formatter
+    console_handler.setLevel(logging.INFO)
+    logfile = 'nwafu.log'
+    File_handler = logging.FileHandler(logfile, mode='a', encoding='utf-8')
+    File_handler.setFormatter(formatter)
+    File_handler.setLevel(logging.DEBUG)
+    if not logger.handlers:
+        logger.addHandler(File_handler)
+        logger.addHandler(console_handler)
+        print(logger.handlers)
+    return logger
+
+
+async def browser_init(**kwargs) -> Optional[Browser]:
+    global _browser
+    try:
+        browser = await async_playwright().start()
+        _browser = await browser.chromium.launch(**kwargs)
+        return _browser
+    except NotImplementedError:
+        logger.warning("windows环境下 初始化playwright失败，相关功能将被限制....")
+    except Exception as e:
+        logger.warning(f"启动chromium发生错误 {type(e)}：{e}")
+        try:
+            if _browser:
+                await _browser.close()
+        except NameError:
+            logger.info("正在安装浏览器...")
+            os.system("playwright install chromium")
+    return None
+
+
+async def async_w(file, content) -> None:
+    """
+    异步写入文件
+    :param file: 文件
+    :param content: 内容
+    :return:
+    """
+    async with aiofiles.open(file, 'w', encoding='utf-8') as f:
+        await f.write(content)
+        await f.close()
+
+
+async def async_r(file):
+    """
+    异步读取文件
+    :param file: 文件
+    :return: 内容
+    """
+    async with aiofiles.open(file, 'r', encoding='utf-8') as f:
+        content = await f.read()
+        await f.close()
+        return content
+
+
+async def init(**kwargs) -> Optional[Browser]:
+    global _browser
+    try:
+        browser = await async_playwright().start()
+        _browser = await browser.chromium.launch(**kwargs)
+        return _browser
+    except NotImplementedError:
+        logger.warning("win环境下 初始化playwright失败，相关功能将被限制....")
+    except Exception as e:
+        logger.warning(f"启动chromium发生错误 {type(e)}：{e}")
+        try:
+            if _browser:
+                await _browser.close()
+        except NameError:
+            logger.info("正在安装浏览器...")
+            os.system("playwright install chromium")
+    return None
+
+
+async def cue_user(event: Event, qq: Optional[int]) -> MessageSegment:
+    """
+    根据不同事件返回不同的用户提醒方式
+    :param event:
+    :param qq:
+    :return:
+    """
+    if isinstance(event, GroupMessageEvent):
+        return MessageSegment.at(qq)
+    elif isinstance(event, PrivateMessageEvent):
+        return MessageSegment.text("")
+
+
+async def plugin_init():
+    """
+    初始化插件
+    :return:
+    """
+    Path.mkdir(LOCAL) if not Path.exists(LOCAL) else ...
+    Path.mkdir(IMG_OUTPUT_PATH) if not Path.exists(IMG_OUTPUT_PATH) else ...
+    Path.mkdir(OUTPUT_PATH) if not Path.exists(OUTPUT_PATH) else ...
+    Path.mkdir(VERIFY_PATH) if not Path.exists(VERIFY_PATH) else ...
+    # LOCAL目录 下是否有 xlsx 文件 或者 xls 文件
+    for i in list(LOCAL.iterdir()):
+        if i.suffix in ['.xlsx', '.xls']:
+            try:
+                workbook_ = load_workbook(i)
+                sheet_names = workbook_.sheetnames
+                sheet = workbook_[sheet_names[0]]
+                youth_qq_info={}
+                for row in sheet.rows:
+                    youth_qq_info[row[0].value] = row[1].value
+                workbook_.close()
+                await async_w(YOUTH_QQ_PATH,json.dumps(youth_qq_info,ensure_ascii=False))
+                break
+            except:
+                logger.info("读取储存有qq信息的本地excel文件失败")
+                if YOUTH_QQ_PATH.exists():
+                    YOUTH_QQ_PATH.unlink()
+    logger.success("陕西共青团插件初始化检测完成")
+
+
+# async def render_and_shoot(temp_path: str, data, html_write: Union[Path, str], img_output: Union[Path, str]) -> None:
+#     env = Environment(loader=FileSystemLoader(str(dirname(__file__))))
+#     template = env.get_template(temp_path)
+#     html = template.render(data=data)
+#     with open(html_write, 'w', encoding='utf-8') as f:
+#         f.write(html)
+#         f.close()
+#     browser = await browser_init()
+#     context = await browser.new_context(locale="zh-CN")
+#     page = await context.new_page()
+#     await page.goto(f"file:///{Path(html_write).resolve()}")
+#     await page.locator('.mdui-table').screenshot(path=img_output)
+
+
+def signal_handler(signum, frame):
+    print('signal_handler: caught signal ' + str(signum))
+    if signum == signal.SIGINT.value:
+        print('操作被用户停止...')
+        sys.exit(1)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+
+def time_log(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        import time
+        start_time = time.time()
+        data = await func(*args, **kwargs)
+        end_time = time.time()
+        logger.info(f"运行时间：{end_time - start_time}")
+        return data
+
+    return wrapper
+
+
+async def youth_analyze(data: dict):
+    """
+    共青团青年大学习分析
+    :param data:
+    :return:
+    """
+    logger.info("正在分析大学习数据...")
+    data = data['data']['data']
+    isStudy = []
+    unfinished = []
+    for youth in data:
+        if youth['isStudy'] == '是':
+            isStudy.append(youth['realname'])
+        elif youth['isStudy'] == '否':
+            unfinished.append(youth['realname'])
+    return isStudy, unfinished
---
+++ youth/utils_net.py
@@ -0,0 +1,123 @@
+# python3
+# -*- coding: utf-8 -*-
+# @Time    : 2022/9/6 20:10
+# @Author  : yzyyz
+# @Email   :  youzyyz1384@qq.com
+# @File    : utils_net.py
+# @Software: PyCharm
+import os
+import json
+from typing import Dict, Union, Optional, Tuple
+
+from httpx import Response
+from playwright._impl._api_types import TimeoutError
+
+import httpx
+from playwright.async_api import async_playwright, Browser, BrowserContext, Page
+from nonebot import logger
+from .utils import *
+from .path import *
+
+
+async def httpx_get(url: str, headers: Dict = None, cookies: Dict[str, str] = None, params: Dict = None) -> Optional[
+    Response]:
+    """
+    httpx get请求
+    :param headers: 请求头
+    :param params: 查询参数
+    :param url: 请求地址
+    :param cookies: cookies
+    :return:
+    """
+    try:
+        async with httpx.AsyncClient(cookies=cookies, headers=headers) as client:
+            r = await client.get(url, params=params)
+            return r
+    except Exception as e:
+        logger.warning(f"请求失败 {type(e)}：{e}")
+        return None
+
+
+async def httpx_post(
+        url: str,
+        headers: Dict[str, str] = None,
+        cookies: Dict[str, str] = None,
+        data: Dict[str, str] = None,
+        type_: str = "json",
+) -> Optional[str]:
+    """
+    httpx post请求
+    :param headers: headers
+    :param url: 请求地址
+    :param cookies: cookies
+    :param data: post数据
+    :param type_: 返回类型
+    :return:
+    """
+    try:
+        async with httpx.AsyncClient(cookies=cookies, headers=headers) as client:
+            r = await client.post(url, data=data)
+            if type_ == "json":
+                return r.json()
+            else:
+                return r.text
+    except Exception as e:
+        logger.warning(f"请求失败 {type(e)}：{e}")
+        return None
+
+
+@time_log
+async def get_main_cookies(context: BrowserContext, page: Page, account: str, password: str, verify: str):
+    """
+    获取主站cookies
+    :return:
+    """
+
+    try:
+        await page.locator("[placeholder=\"请输入账号\"]").fill(account)
+        await page.locator("[placeholder=\"请输入密码\"]").fill(password)
+        await page.locator("[placeholder=\"请输入验证码\"]").fill(verify)
+        await page.locator("button:has-text(\"登 录\")").click()
+        await page.wait_for_url("https://www.sxgqt.org.cn/bgsxv2/reunion/member/groupFareManagement")
+        html = await page.content()
+        await context.storage_state(path=AUTH_PATH)
+        await page.close()
+        auth = json.loads(AUTH_PATH.read_text())
+        cookies = auth["cookies"]
+        getToken = False
+        token = None
+        for cookie in cookies:
+            if cookie["name"] == "token":
+                getToken = True
+                token = cookie["value"]
+        if getToken:
+            return token
+        else:
+            logger.warning("登陆失败")
+
+        await context.close()
+
+
+    except TimeoutError:
+        logger.warning("操作超时，请重试")
+        raise OperationTimedOutError
+
+
+# context: BrowserContext,
+async def playwright_login() -> Union[tuple[BrowserContext, Page], str]:
+    browser = await browser_init()
+    context = await browser.new_context(locale="zh-CN")
+    page = await context.new_page()
+    try:
+        await page.goto("https://www.sxgqt.org.cn/bgsxv2/login?redirect=%2Freunion%2Fmember%2FgroupFareManagement")
+        img = await page.locator('//*[@id="pane-first"]/div/div/form/div[3]/div/img').screenshot(type="jpeg",
+                                                                                                 path=VERIFY_PATH / "verify.jpg",
+                                                                                                 quality=100)
+        return context, page
+
+    except TimeoutError:
+        return "TimeoutError"
+
+
+class OperationTimedOutError(Exception):
+    pass
